200ca4b7e25c235281eec38158ab81fb13e680b8
Abdullah Khan <khanabdullah080@gmail.com>
2026-02-11 12:24:20 +1100

    feat: add Cloudflare plugin and update package.json for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
+import { cloudflare } from '@cloudflare/vite-plugin';
 
 const config = defineConfig({
   resolve: {
@@ -15,8 +16,8 @@ const config = defineConfig({
     },
   },
   plugins: [
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     devtools(),
-    // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ["./tsconfig.json"],
     }),
