913c21d3fb1a65f83d40b48b21775b2a7fbdb865
Abdullah Khan <khanabdullah080@gmail.com>
2026-02-14 22:44:41 +1100

    feat(ui): add form component

---
+++ src/components/ui/form.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { Form as FormPrimitive } from "@base-ui/react/form";
+import { cn } from "@/lib/utils";
+
+function Form({ className, ...props }: FormPrimitive.Props) {
+  return <FormPrimitive className={cn("", className)} {...props} />;
+}
+
+export { Form };
