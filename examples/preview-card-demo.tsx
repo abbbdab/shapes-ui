fec72678de2be3ba563c639d87c7a23c5aba82a9
Abdullah Khan <khanabdullah080@gmail.com>
2026-02-16 21:42:49 +1100

    feat: Add examples for PreviewCard component and enhance Popup functionality

--- examples/preview-card-demo.tsx
+++ examples/preview-card-demo.tsx
@@ -1,3 +1,28 @@
+import { PreviewCard, PreviewCardPopup, PreviewCardTrigger } from "@/components/ui/preview-card";
+
 export default function PreviewCardDemo() {
-  return <div></div>;
+  return (
+    <PreviewCard>
+      <p className={' text-sm'}>
+        Apple's{" "}
+        <PreviewCardTrigger
+          target={"_blank"}
+          href={"https://developer.apple.com/documentation/TechnologyOverviews/liquid-glass"}
+        >
+          Liquid Glass
+        </PreviewCardTrigger>{" "}
+        has been a controversial release.
+      </p>
+      <PreviewCardPopup className={"flex w-fit flex-col gap-2 items-center justify-center p-3"}>
+        <img
+          alt={"Liquid Glass UI"}
+          className={"w-72 rounded-lg bg-muted object-cover p-2"}
+          src={
+            "https://docs-assets.developer.apple.com/published/2299e5e8eafedded6947927a8f704c34/landing-page-intoducing-liquid-glass-hero~dark%402x.png"
+          }
+        />
+        <p className={"text-xs"}>Liquid glass affects SwiftUI, UIKit, and AppKit.</p>
+      </PreviewCardPopup>
+    </PreviewCard>
+  );
 }
---
+++ examples/preview-card-links.tsx
@@ -0,0 +1,38 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card.tsx";
+import { Meter } from "@/components/ui/meter.tsx";
+import { PreviewCard, PreviewCardPopup, PreviewCardTrigger } from "@/components/ui/preview-card";
+import { Button } from "@/components/ui/button.tsx";
+import { Badge } from "@/components/ui/badge.tsx";
+
+export default function PreviewCardLinks() {
+  return (
+    <PreviewCard>
+      <Card size={"sm"} className={"w-md"}>
+        <CardHeader className={"flex flex-row items-center justify-between"}>
+          <CardTitle>Quote usage</CardTitle>
+          <Badge variant={'warning'}>Basic</Badge>
+        </CardHeader>
+        <CardContent className={'space-y-3'}>
+          <Meter value={72} className={"w-full"} />
+          <div className={'flex flex-col'}>
+            <CardDescription>
+              72 out of 100 requests used this <PreviewCardTrigger>month.</PreviewCardTrigger>.
+            </CardDescription>
+          </div>
+        </CardContent>
+      </Card>
+      <PreviewCardPopup side={"bottom"} className={'w-sm p-4'}>
+        <div className="flex flex-col gap-2">
+          <p className={'text-xs text-muted-foreground'}>Usage resets {new Date(Date.now() + 4 * 7 * 24 * 60 * 60 * 1000).toLocaleDateString()} (4 weeks from now).</p>
+          <Button variant={'secondary'} className={'ml-auto'} size={'sm'}>Upgrade to Pro</Button>
+        </div>
+      </PreviewCardPopup>
+    </PreviewCard>
+  );
+}
